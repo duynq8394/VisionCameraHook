9c5f2ca6bcb732dcd77eb4da1aa5ec3627462e12
duynq8394 <duynq8394@gmail.com>
2026-07-19 14:03:41 +0700

    Update VisionCameraHook.m

--- VisionCameraHook.m
+++ VisionCameraHook.m
@@ -1,9 +1,13 @@
 #import <UIKit/UIKit.h>
+#import <PhotosUI/PhotosUI.h>
 #import <objc/runtime.h>
 
-static BOOL gShown = NO;
+typedef void (^RCTPromiseResolveBlock)(id result);
+typedef void (^RCTPromiseRejectBlock)(NSString *code, NSString *message, NSError *error);
 
-#pragma mark - Top View Controller
+static void (*orig_takePhoto)(id, SEL, id, id, RCTPromiseResolveBlock, RCTPromiseRejectBlock);
+
+#pragma mark - Top ViewController
 
 static UIViewController *TopVC(void) {
 
@@ -34,12 +38,85 @@ static UIViewController *TopVC(void) {
     return nil;
 }
 
-#pragma mark - Inspector
+#pragma mark - Picker Delegate
 
-static void ShowInspector(void) {
+@interface VC_PickerDelegate : NSObject
+<UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 
-    if (gShown) return;
-    gShown = YES;
+@property (nonatomic, copy) RCTPromiseResolveBlock resolve;
+@property (nonatomic, copy) RCTPromiseRejectBlock reject;
+
+@end
+
+@implementation VC_PickerDelegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary *)info {
+
+    UIImage *image = info[UIImagePickerControllerOriginalImage];
+
+    NSString *path = [NSTemporaryDirectory() stringByAppendingPathComponent:@"vc_hook.jpg"];
+    NSData *data = UIImageJPEGRepresentation(image, 0.9);
+    [data writeToFile:path atomically:YES];
+
+    [picker dismissViewControllerAnimated:YES completion:nil];
+
+    if (self.resolve) {
+        self.resolve(@{
+            @"path": path
+        });
+    }
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+
+    [picker dismissViewControllerAnimated:YES completion:nil];
+
+    if (self.reject) {
+        self.reject(@"cancelled", @"User cancelled", nil);
+    }
+}
+
+@end
+
+static VC_PickerDelegate *gDelegate;
+
+#pragma mark - Hook Implementation
+
+static void hook_takePhoto(id self,
+                           SEL _cmd,
+                           id arg1,
+                           id arg2,
+                           RCTPromiseResolveBlock resolve,
+                           RCTPromiseRejectBlock reject)
+{
+    NSLog(@"📸 takePhoto intercepted -> Using Photo Library");
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+
+        UIViewController *vc = TopVC();
+        if (!vc) {
+            if (reject)
+                reject(@"no_vc", @"No active ViewController", nil);
+            return;
+        }
+
+        UIImagePickerController *picker = [UIImagePickerController new];
+        picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+
+        gDelegate = [VC_PickerDelegate new];
+        gDelegate.resolve = resolve;
+        gDelegate.reject = reject;
+
+        picker.delegate = gDelegate;
+
+        [vc presentViewController:picker animated:YES completion:nil];
+    });
+}
+
+#pragma mark - Install Hook
+
+static void InstallHook(void) {
 
     Class cls = objc_getClass("CameraViewManager");
     if (!cls) {
@@ -47,63 +124,25 @@ static void ShowInspector(void) {
         return;
     }
 
-    unsigned int count = 0;
-    Method *methods = class_copyMethodList(cls, &count);
+    SEL sel = sel_getUid("takePhoto:options:resolve:reject:");
+    Method m = class_getInstanceMethod(cls, sel);
 
-    NSMutableString *output =
-    [NSMutableString stringWithString:@"CameraViewManager\n\n"];
-
-    for (unsigned int i = 0; i < count; i++) {
-
-        SEL sel = method_getName(methods[i]);
-        NSString *name = NSStringFromSelector(sel);
-
-        if ([name containsString:@"takePhoto"]) {
-
-            const char *types = method_getTypeEncoding(methods[i]);
-
-            [output appendFormat:@"SEL: %@\n", name];
-            [output appendFormat:@"TypeEncoding: %s\n\n", types];
-        }
+    if (!m) {
+        NSLog(@"❌ Method not found");
+        return;
     }
 
-    free(methods);
+    orig_takePhoto = (void *)method_getImplementation(m);
+    method_setImplementation(m, (IMP)hook_takePhoto);
 
-    dispatch_async(dispatch_get_main_queue(), ^{
-
-        UIViewController *vc = TopVC();
-        if (!vc) return;
-
-        UIAlertController *alert =
-        [UIAlertController alertControllerWithTitle:@"Method Inspector"
-                                            message:output
-                                     preferredStyle:UIAlertControllerStyleAlert];
-
-        [alert addAction:[UIAlertAction actionWithTitle:@"OK"
-                                                  style:UIAlertActionStyleCancel
-                                                handler:nil]];
-
-        [vc presentViewController:alert animated:YES completion:nil];
-    });
+    NSLog(@"✅ takePhoto hook installed");
 }
 
 #pragma mark - Entry
 
 __attribute__((constructor))
 static void Init(void) {
-
     dispatch_async(dispatch_get_main_queue(), ^{
-
-        if (UIApplication.sharedApplication.applicationState == UIApplicationStateActive) {
-            ShowInspector();
-        }
-
-        [[NSNotificationCenter defaultCenter]
-         addObserverForName:UIApplicationDidBecomeActiveNotification
-         object:nil
-         queue:[NSOperationQueue mainQueue]
-         usingBlock:^(NSNotification * _Nonnull note) {
-            ShowInspector();
-        }];
+        InstallHook();
     });
 }
